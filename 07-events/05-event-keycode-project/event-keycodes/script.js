5dcd0e7b731415917d8a099e9bd770870f3baece
Karan Meghe <karanmeghe87@gmail.com>
2024-03-09 00:40:50 +0530

    Update: data mode get updated

--- 07-events/05-event-keycode-project/event-keycodes/script.js
+++ 07-events/05-event-keycode-project/event-keycodes/script.js
@@ -21,35 +21,63 @@
 // });
 
 // Method 2
+// const showKeyCodes = (e) => {
+//   const insert = document.querySelector("#insert");
+//   insert.innerHTML = " ";
+
+//   const keysInfo = {
+//     key: e.key,
+//     keyCode: e.keyCode,
+//     code: e.code,
+//   };
+
+//   for (const key in keysInfo) {
+//     const div = document.createElement("div");
+//     div.className = "key";
+
+//     const small = document.createElement("small");
+//     const keyText = document.createTextNode(
+//       keysInfo[key] === " " ? "Space" : keysInfo[key]
+//     );
+//     const keyValue = document.createTextNode(key);
+
+//     small.appendChild(keyValue);
+//     div.appendChild(keyText);
+//     div.appendChild(small);
+//     insert.appendChild(div);
+
+//     div.style.marginBottom = "40px";
+
+//     console.log(div);
+//   }
+// };
+
 const showKeyCodes = (e) => {
-  const insert = document.querySelector("#insert");
+  const insert = document.getElementById("insert");
   insert.innerHTML = " ";
 
-  const keysInfo = {
-    key: e.key,
-    keyCode: e.keyCode,
-    code: e.code,
-  };
+  const keysInfo = [
+    {
+      "e.key": e.key,
+      "e.keyCode": e.keyCode,
+      "e.code": e.code,
+    },
+  ];
 
-  for (const key in keysInfo) {
-    const div = document.createElement("div");
-    div.className = "key";
+  keysInfo.forEach((object) => {
+    for (const key in object) {
+      const div = document.createElement("div");
+      const small = document.createElement("small");
 
-    const small = document.createElement("small");
-    const keyText = document.createTextNode(
-      keysInfo[key] === " " ? "Space" : keysInfo[key]
-    );
-    const keyValue = document.createTextNode(key);
-
-    small.appendChild(keyValue);
-    div.appendChild(keyText);
-    div.appendChild(small);
-    insert.appendChild(div);
-
-    div.style.marginBottom = "40px";
-
-    console.log(div);
-  }
+      div.className = "key";
+      const keys = document.createTextNode(object[key]);
+      const keysValue = document.createTextNode(key);
+      small.appendChild(keys);
+      div.appendChild(keysValue);
+      div.appendChild(small);
+      insert.appendChild(div);
+    }
+  });
 };
 
 window.addEventListener("keydown", showKeyCodes, false);
