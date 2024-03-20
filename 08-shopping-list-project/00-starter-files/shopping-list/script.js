96ba9f4e961231dd23de4db68e0cc16d1d83e7ab
Karan Meghe <karanmeghe87@gmail.com>
2024-03-20 14:17:48 +0530

    feat - adding , remvoving items from local storage

--- 08-shopping-list-project/00-starter-files/shopping-list/script.js
+++ 08-shopping-list-project/00-starter-files/shopping-list/script.js
@@ -6,6 +6,13 @@ const addItemBtn = document.querySelector(".btn");
 const filter = document.querySelector(".filter input");
 const clearBtn = document.querySelector("#clear");
 
+// displayItems
+function displayItems() {
+  const itemsFromStorage = getItemsFromStorage();
+  itemsFromStorage.forEach((item) => addItemToDom(item));
+  checkUi();
+}
+
 // warning message
 const warningMessage = () => {
   const warningText = document.createElement("p");
@@ -35,6 +42,29 @@ const createButton = (className) => {
   return button;
 };
 
+// Get Value From Local Storage
+const getItemsFromStorage = () => {
+  let itemsForStorage;
+  // Get values from Local Storage but first check weather its empty or not
+  if (JSON.parse(localStorage.getItem("items") === null)) {
+    itemsForStorage = []; // on initial stage there will be no data so it will be empty array.
+  } else {
+    itemsForStorage = JSON.parse(localStorage.getItem("items")); // on 1st render it will be not empty so we are getting data
+  }
+  return itemsForStorage;
+};
+
+// Local Storage
+const addItemsToStorage = (item) => {
+  // creating itemsForStorage variable for having scope for this function, block
+  const itemsForStorage = getItemsFromStorage();
+
+  itemsForStorage.push(item); // pushing item into array
+
+  // Convert to JSON string and set items into local storage
+  localStorage.setItem("items", JSON.stringify(itemsForStorage));
+};
+
 const onSubmit = (e) => {
   // prevent default behaviour of the form
   e.preventDefault();
@@ -55,15 +85,14 @@ const onSubmit = (e) => {
     }
   }
 
-  // Create list item if newItem is not empty
-  if (newItem !== "") {
-    const item = document.createElement("li");
-    item.textContent = newItem;
-    itemList.appendChild(item);
+  //   const itemsFromStorage = displayItems();
 
-    const button = createButton("remove-item btn-link text-red");
-    item.appendChild(button);
-  }
+  // Create list item if newItem is not empty
+  //   Add item to Dom
+  addItemToDom(newItem);
+
+  addItemsToStorage(newItem);
+
   formInput.value = "";
 
   // toggle filter and clear all button only when user add item to list
@@ -72,6 +101,17 @@ const onSubmit = (e) => {
     clearBtn.style.display = "block";
   }
 };
+
+function addItemToDom(newItem) {
+  if (newItem !== "") {
+    const item = document.createElement("li");
+    item.textContent = newItem;
+    itemList.appendChild(item);
+
+    const button = createButton("remove-item btn-link text-red");
+    item.appendChild(button);
+  }
+}
 
 // Check Ui
 const checkUi = () => {
@@ -92,18 +132,56 @@ const clearItems = () => {
   //   e.stopPropagation();
 };
 
-// Remove single Element (event delegation)
-const removeItem = (e) => {
+// onClick Item
+function onClickItem(e) {
   if (e.target.parentElement.classList.contains("remove-item")) {
-    e.target.parentElement.parentElement.remove();
-    checkUi();
+    removeItem(e.target.parentElement.parentElement);
   }
+}
+
+// Remove single Element (event delegation)
+const removeItem = (item) => {
+  // Remove item from dom
+  item.remove();
+  // Remove item from localStorage
+  removeItemsFromStore(item.textContent);
+  checkUi();
 };
 
-// Event Listner
-form.addEventListener("submit", onSubmit, false);
-clearBtn.addEventListener("click", clearItems, false);
-itemList.addEventListener("click", removeItem, false);
+// rmoving item from local storage
+function removeItemsFromStore(item) {
+  let itemFromStorage = getItemsFromStorage();
+  itemFromStorage = itemFromStorage.filter((i) => i !== item);
 
-console.dir(itemList);
-checkUi();
+  //   Re-set Local storage
+  localStorage.setItem("items", JSON.stringify(itemFromStorage));
+}
+
+// Filter items
+const filterItems = (e) => {
+  const inputText = e.target.value.toLowerCase();
+  const items = document.querySelectorAll("li");
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.indexOf(inputText) !== -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+};
+
+//
+// Event Listner, Initialize App
+function init() {
+  form.addEventListener("submit", onSubmit, false);
+  clearBtn.addEventListener("click", clearItems, false);
+  itemList.addEventListener("click", onClickItem, false);
+  filter.addEventListener("input", filterItems, false);
+  document.addEventListener("DOMContentLoaded", displayItems, false);
+  checkUi();
+}
+
+init();
